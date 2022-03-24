1c43c9b8820ba0a70c4779606608ad3c0bc6d7df
julia <juliamarcal12@gmail.com>
2022-03-24 18:04:06 -0300

    add PHP project API

---
+++ API em php com laravel/teknisa-dev-api/database/migrations/2022_03_24_182427_create_devs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('age');
+            $table->string('picture');
+            $table->text('programmingLanguages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devs');
+    }
+}
